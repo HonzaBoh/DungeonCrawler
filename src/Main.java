381d6766e7665e774d32db24eba2c5d7a35cd0de
bohac <honza.boh00@gmail.com>
2024-05-16 08:59:22 +0200

    One last line added

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("New line to be showed within git");
+        System.out.println("One last line");
     }
 }
