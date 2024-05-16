b0c5c532d4f17f0fcb8c7b27ef70d4fe191dbec6
bohac <honza.boh00@gmail.com>
2024-05-16 09:00:40 +0200

    Cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("New line to be showed within git");
-        System.out.println("One last line");
     }
 }
