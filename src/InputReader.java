69b1a48cb9c0749160cf829e7e753ae9d992c08f
bohac <honza.boh00@gmail.com>
2024-05-16 09:36:20 +0200

    Cleanup and final vcs setup

---
+++ src/InputReader.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class InputReader {
+    private static final Scanner sc = new Scanner(System.in);
+
+    static String readText(String prompt){
+        System.out.println(prompt);
+        return sc.nextLine();
+    }
+
+    static int readChoice(String[] choices){
+        int choice;
+        do {
+            System.out.println("Vyber možnost:");
+            for (int i = 0; i < choices.length; i++) {
+                System.out.println((i+1) + ": " + choices[i]);
+            }
+            choice = sc.nextInt()-1;
+        }while (choice < 0 || choice >= choices.length);
+        return choice;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("VCS setup done...hopefully");
+
+        //testing:
+        String readMe = InputReader.readText("Zadej text k precteni:");
+        System.out.println("Zadano: " + readMe);
+
+        int readChoice = InputReader.readChoice(new String[]{"Neco", "Dalsi", "Na ukazku"});
+        System.out.println("Vybrana moznost: " + readChoice);
     }
 }
